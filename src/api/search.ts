5612ab87bba85699654be353591ce0f5b6357454
una3325 <a33256448@gmail.com>
2025-06-10 02:14:29 +0900

    refactor: axios baseURL 적용

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,26 +1,34 @@
 import axios from 'axios';
 import type { PropertyCardData } from '../types/property';
 
+const baseURL = 'http://127.0.0.1:8000';
+
+
+const api = axios.create({
+  baseURL,
+  withCredentials: false,
+});
+
 interface PropertySearchResponse {
   results: PropertyCardData[];
 }
 
 export async function fetchSearchResults(keyword: string): Promise<PropertyCardData[]> {
-  const response = await axios.get<PropertySearchResponse>('http://127.0.0.1:8000/search', {
+  const response = await api.get<PropertySearchResponse>('/search', {
     params: { keyword },
   });
   return response.data.results;
 }
 
 export async function fetchPropertyDetails(keyword: string) {
-  const response = await axios.get('http://127.0.0.1:8000/complex-info', {
+  const response = await api.get('/complex-info', {
     params: { keyword },
   });
   return response.data;
 }
 
 export async function fetchPricePrediction(keyword: string, area: string, deal_type: string) {
-  const response = await axios.get('http://127.0.0.1:8000/all-info', {
+  const response = await api.get('/all-info', {
     params: {
       keyword,
       area: area.replace('㎡', ''),
@@ -31,16 +39,16 @@ export async function fetchPricePrediction(keyword: string, area: string, deal_t
 }
 
 export async function fetchAllHistory(): Promise<PropertyCardData[]> {
-  const res = await axios.get('http://127.0.0.1:8000/apartments');
+  const res = await api.get('/apartments');
   return res.data;
 }
 
 export async function fetchHistoryDetail(id: number): Promise<PropertyCardData> {
-  const res = await axios.get(`http://127.0.0.1:8000/apartments/${id}`);
+  const res = await api.get(`/apartments/${id}`);
   return res.data;
 }
 
 export async function deleteHistoryDetail(id: number): Promise<PropertyCardData> {
-  const res = await axios.delete(`http://127.0.0.1:8000/apartments/${id}`);
+  const res = await api.delete(`/apartments/${id}`);
   return res.data;
 }
